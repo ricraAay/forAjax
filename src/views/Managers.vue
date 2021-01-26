5255baa215f986ff10c93bcb1f64925ce8b8afd9
ricraAay <riicray@gamil.com>
2021-01-26 17:16:41 +0300

    install vuetify

---
+++ src/views/Managers.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page-managers">
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+/* import HelloWorld from '@/components/HelloWorld.vue';  */
+
+@Component({
+  components: {
+    /* HelloWorld, */
+  },
+})
+export default class Managers extends Vue {
+  items: Array<object> = []
+}
+</script>
